a9d092edafe8251e2ce516e1b246c5feb88c7c6b
muhammad elalfy <eng_mohamedelalfy@yahoo.com>
2020-10-14 05:52:10 +0200

    login and register with mobile

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,8 +38,6 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function username()
-    {
-        return 'mobile';
-    }
+
+
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">mobile</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Mobile</label>
 
                             <div class="col-md-6">
                                 <input id="mobile" type="text" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" autofocus>
